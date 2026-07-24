15e80708d126296d6235a6c327c0230a841d1997
Kevin Barnasthpol <kevin.barnasthpol@gmail.com>
2026-07-24 19:59:49 -0300

    cambiado texto 5

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -46,61 +46,93 @@ export default function Hero() {
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_bottom_left,rgba(246,174,45,0.05),transparent_80%)]" />
 
       <div className="container mx-auto relative z-10">
-        <h1
-  className={`
-    font-extrabold tracking-tight text-principal
-    transition-all duration-700 ease-out
-    ${
-      compact
-        ? "scale-85 opacity-20 translate-y-4"
-        : "scale-100 opacity-100 translate-y-0"
-    }
-  `}
->
-  <span className="block text-5xl sm:text-5xl md:text-8xl">
-    BIASI
-  </span>
 
-  <span className="block md:inline text-2xl sm:text-3xl md:text-8xl">
-    <span className="text-detalles">&</span>ASOCIADOS
-  </span>
-</h1>
+  {/* ================= DESKTOP ================= */}
+  <div className="hidden md:block">
+    <h1
+      className={`
+        text-8xl
+        font-extrabold
+        tracking-tight
+        text-principal
+        transition-all duration-700 ease-out
+        ${
+          compact
+            ? "scale-85 opacity-20 translate-y-4"
+            : "scale-100 opacity-100 translate-y-0"
+        }
+      `}
+    >
+      BIASI<span className="text-detalles">&</span>ASOCIADOS
+    </h1>
+  </div>
 
-        <div
-          className={`
-h-1
-bg-detalles
-mx-auto
-mb-6
-transition-all
-duration-1500
-${compact
-              ? "w-50 opacity-40"
-              : "md:w-200 sm:w-full opacity-100"
-            }
-`}
-        />
+  {/* ================= MOBILE ================= */}
+  <div
+    className={`
+      md:hidden
+      w-full
+      -translate-y-10
+      transition-all duration-700 ease-out
+      ${
+        compact
+          ? "scale-85 opacity-20 translate-y-4"
+          : "scale-100 opacity-100"
+      }
+    `}
+  >
+    <h1 className="text-left leading-none">
+      <span className="block text-8xl font-extrabold text-principal">
+        BIASI
+      </span>
 
-        <h2 className={`
-text-base
-sm:text-xl
-md:text-2xl
-font-bold
-tracking-widest
-uppercase
-leading-relaxed
-text-principal
-transition-all
-duration-700
-${compact
-            ? "opacity-0 translate-y-4"
-            : "opacity-100 translate-y-0"
-          }
-`}>
-          Asesoramiento Legal Integral{" "}
-          <span className="text-detalles">&</span> Soluciones Estratégicas
-        </h2>
-      </div>
+      <span className="block text-5xl font-bold text-right text-principal mt-1">
+        <span className="text-detalles">&</span>ASOCIADOS
+      </span>
+    </h1>
+  </div>
+
+  <div
+    className={`
+      h-1
+      bg-detalles
+      mx-auto
+      mb-6
+      transition-all
+      duration-1500
+      ${
+        compact
+          ? "w-50 opacity-40"
+          : "md:w-200 w-full opacity-100"
+      }
+    `}
+  />
+
+  <h2
+    className={`
+      text-base
+      sm:text-xl
+      md:text-2xl
+      font-bold
+      tracking-widest
+      uppercase
+      leading-relaxed
+      text-principal
+      transition-all
+      duration-700
+      text-right
+      ${
+        compact
+          ? "opacity-0 translate-y-4"
+          : "opacity-100 translate-y-0"
+      }
+    `}
+  >
+    Asesoramiento Legal Integral{" "}
+    <span className="text-detalles">&</span> Soluciones Estratégicas
+  </h2>
+
+</div>
     </div>
   );
 }
